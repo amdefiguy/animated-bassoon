8d6aff1c815d1099af6507e536fddff61e0d04e5
DefiGuy <32579720+amdefiguy@users.noreply.github.com>
2022-05-01 17:08:24 +0300

    Update timer.py

--- timer.py
+++ timer.py
@@ -4,7 +4,7 @@ localtime = time.asctime( time.localtime(time.time()) )
 
 execTime = localtime
 
-file = open("readme.md", 'w')
+file = open("readme.txt", 'w')
 file.write("Kept code alive at : ")
 file.write(str(execTime))
 file.close()
